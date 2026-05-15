0a593762d7717a423b92027fc99693b8d9e6bd95
Sathv <sathv@example.com>
2026-05-15 12:12:34 +0530

    Fix firebase config format

--- frontend/firebase-config.js
+++ frontend/firebase-config.js
@@ -1,10 +1,4 @@
-﻿// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+window.FIREBASE_CONFIG = {
     apiKey: "AIzaSyBbcPxLYUEUKf8pLac_--xpoRPzzuGgicI",
     authDomain: "accessguard-cda57.firebaseapp.com",
     projectId: "accessguard-cda57",
@@ -12,6 +6,3 @@ const firebaseConfig = {
     messagingSenderId: "833285517019",
     appId: "1:833285517019:web:83ed48093fcd50563666fe"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
